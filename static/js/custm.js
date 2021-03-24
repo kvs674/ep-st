127ad798e58851034e52df6fa56251ba385abd52
Kuchni <65308606+kvs674@users.noreply.github.com>
2021-03-24 15:47:14 +0300

    Update custm.js

--- static/js/custm.js
+++ static/js/custm.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	if ($(window).width() < 560) {
 $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 975) {
@@ -11,37 +12,75 @@ $(window).scroll(function() {
         } else {
             $(".player").css("padding-bottom", "0");
         }
-    });
+    });}
 	$('.load-more').click(function(){
 	$( ".load-more" ).remove();
 	});
-	
-$("#check1").each(function() {
+$('.valueup').click(function(){
+    $('.network').toggle();
+});
+$('.languages').click(function(){
+    $('.dropdown-content').toggle();
+});
+$(document).on('click', function(event) {
+	if (!$(event.target).closest('.valueup, #usermobile, .languages').length) {
+		$('.network').hide();
+		$('.mobile-menu').hide();
+		$('.dropdown-content').hide();
+	}
+});
+$('.dropdown-content li').click(function() {
+	$.cookie('kt_lang', $(this).attr('data-value'), {expires: 360, path: '/'});
+	window.location.reload();
+});
+$('.mobile-menu').hide();
+if ($(window).width() < 785) {
+     $('.search').hide();
+    }
+$('.search-mobile-icon').click(function(){
+  $('.search').insertAfter(".header-inner");
+  $('.search').toggle();
+});	
+$('#usermobile').click(function(){
+$('.mobile-menu').toggle();
+});	
+$("#checkbox").each(function() {
     var mycookie = $.cookie($(this).attr('name'));
     if (mycookie && mycookie == "true") {
         $(this).prop('checked', mycookie);
     }
 });
-$("#check1").change(function() {
+$("#checkbox").change(function() {
     $.cookie($(this).attr("name"), $(this).prop('checked'), {
         path: '/',
-        expires: 365
+        expires: 360
     });
-});
-	
+});	
 if ($.cookie('kt_rt_skin') != '1')
     {
-$("#check1").click(function() {
-$('#check1').attr('checked', true);
+$("#checkbox").click(function() {
+$('#checkbox').attr('checked', true);
 if($(this).is(':checked'))
   {
-	$.cookie('kt_rt_skin', 'white', {expires: 360, path: '/'});
+	$.cookie('kt_rt_skin', 'dark', {expires: 360, path: '/'});
 window.location.reload();
   }
-
 else {
-	$.cookie('kt_rt_skin', 'dark', {expires: 360, path: '/'});
+	$.cookie('kt_rt_skin', 'white', {expires: 360, path: '/'});
 window.location.reload();}
 });
 }
+$(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.up-arrow').fadeIn();
+        } else {
+            $('.up-arrow').fadeOut();
+        }
+    });
+    $('.up-arrow').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
